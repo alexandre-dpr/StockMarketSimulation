ca026957d34f53cdab169d43c32ba358c055c4e0
Dupre Alexandre <alexandre.dupre@etu.univ-orleans.fr>
2024-02-16 18:57:32 +0100

    Pagination de /tickerAutoComplete

--- bourse/src/main/java/bourse/repository/TickerRepository.java
+++ bourse/src/main/java/bourse/repository/TickerRepository.java
@@ -7,12 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface TickerRepository extends JpaRepository<Ticker, String> {
 
     @Query("SELECT t FROM Ticker t WHERE t.ticker=:name OR t.Name LIKE %:name% ORDER BY LENGTH(t.ticker)")
-    List<Ticker> findBySimilarName(@Param("name") String name);
+    Page<Ticker> findBySimilarName(@Param("name") String name, Pageable pageable);
 
     Page<Ticker> findAll(Pageable pageable);
 }
