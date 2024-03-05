a352477431cbcd832c803fd18d7188a4bea0e85c
Alexandre <alexandre.dupre2001@gmail.com>
2024-03-06 00:09:17 +0100

    [BACK] StockDto history en json

--- bourse/src/main/java/bourse/util/JsonUtils.java
+++ bourse/src/main/java/bourse/util/JsonUtils.java
@@ -10,6 +10,10 @@ public class JsonUtils {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * Converti un objet en json stringified
+     * @param object objet à transformer
+     */
     public static String toJson(Object object) {
         try {
             return objectMapper.writeValueAsString(object);
@@ -18,9 +22,28 @@ public class JsonUtils {
         }
     }
 
-    public static <T> List<T> fromJsonList(String json, Class<T> elementType) {
+    /**
+     * Converti un json stringified dans le type voulu
+     * @param json String qui contient le json
+     * @param targetType type de destination
+     */
+    public static <T> T fromJson(String json, Class<T> targetType) {
         try {
-            CollectionType listType = objectMapper.getTypeFactory().constructCollectionType(List.class, elementType);
+            return objectMapper.readValue(json, targetType);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Erreur lors de la désérialisation JSON", e);
+        }
+    }
+
+
+    /**
+     * Converti un json stringified dans une liste du type voulu
+     * @param json String qui contient le json
+     * @param targetType type de destination
+     */
+    public static <T> List<T> fromJsonList(String json, Class<T> targetType) {
+        try {
+            CollectionType listType = objectMapper.getTypeFactory().constructCollectionType(List.class, targetType);
             return objectMapper.readValue(json, listType);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Erreur lors de la désérialisation JSON de la liste", e);
