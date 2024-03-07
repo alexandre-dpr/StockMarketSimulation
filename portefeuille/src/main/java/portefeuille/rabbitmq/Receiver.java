0fb2134cd068d6561fd6e2a56b51eb21140f2e34
Arezki <arezki.aid@etu-univ.orelans.fr>
2024-03-07 16:27:25 +0100

    full dockerisation

--- portefeuille/src/main/java/portefeuille/rabbitmq/Receiver.java
+++ portefeuille/src/main/java/portefeuille/rabbitmq/Receiver.java
@@ -7,21 +7,26 @@ import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import portefeuille.dto.rabbitMq.TickerInfoDto;
 import portefeuille.modele.TickerInfo;
 import portefeuille.repository.TickerInfoRepository;
+import portefeuille.service.CacheService;
 
 @Component
 public class Receiver implements RabbitListenerConfigurer {
     private static final Logger logger = LoggerFactory.getLogger(Receiver.class);
     @Autowired
+    CacheService cacheService;
+    @Autowired
     TickerInfoRepository repository;
-    @Transactional
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     @RabbitListener(queues = "${spring.rabbitmq.queue}")
     public void receivedMessage(TickerInfoDto ticker) {
         logger.info("Service portfeuille TickerReceived is  " + ticker);
-        repository.save(TickerInfo.builder().uuid(ticker.uuid()).ticker(ticker.ticker()).price(ticker.price()).build());
+ //       cacheService.addTickerInfo(ticker);
+        repository.save(TickerInfo.builder().uuid(ticker.uuid()).price(ticker.price()).ticker(ticker.ticker()).build());
         logger.info("Service portfeuille Ticker saved  " + ticker);
     }
 
---
+++ portefeuille/src/main/java/portefeuille/service/CacheService.java
@@ -0,0 +1,26 @@
+package portefeuille.service;
+
+import org.springframework.stereotype.Service;
+import portefeuille.dto.rabbitMq.TickerInfoDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CacheService {
+    static final private List<TickerInfoDto> cache= new ArrayList<>();
+
+    public void addTickerInfo(TickerInfoDto tickerInfoDto){
+        cache.add(tickerInfoDto);
+    }
+    public TickerInfoDto getTickerInfo(String uuid){
+        return cache.stream().filter(tickerInfoDto -> uuid.equals(tickerInfoDto.uuid())).findAny().get();
+    }
+
+    public void removeTickerInfo(String uuid){
+        cache.removeIf(tickerInfoDto -> uuid.equals(tickerInfoDto.uuid()));
+    }
+    public boolean isPresentTickerInfo(String uuid){
+       return cache.stream().anyMatch(tickerInfoDto -> uuid.equals(tickerInfoDto.uuid()));
+    }
+}
